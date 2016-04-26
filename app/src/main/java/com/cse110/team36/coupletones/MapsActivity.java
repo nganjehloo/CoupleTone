24f1448ea36bee1ee2bbeadb59a94f585eebc7ce
MrSwirlyEyes <cvjkeef@gmail.com>
2016-04-26 13:58:17 -0700

    Added 1/10th radius circle around dropped marker from a long-click, added final constant to represent 1/10th radius, tried to add constant colors in colors.xml, but colours didnt match

--- app/src/main/java/com/cse110/team36/coupletones/MapsActivity.java
+++ app/src/main/java/com/cse110/team36/coupletones/MapsActivity.java
@@ -8,7 +8,10 @@ import android.view.animation.BounceInterpolator;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -16,6 +19,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLongClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
+    final float ONE_TENTH_MILE = 160.934f;  // ONE TENTH OF A MILE (in meters) ***** ADDED BY MrSwirlyEyes 4/26
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,9 +71,13 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
      *
      *****/
     public void onMapLongClick(LatLng point) {
-
+        Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude)));
 //        mMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude)));
-        dropPinEffect(mMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude))));
+        dropPinEffect(marker);
+
+
+        addMarkerCircle(marker);
+
     }
 
 
@@ -109,6 +117,43 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
                 }
             }
         });
+    }
+
+
+    /***** CIRCLE RADIUS AROUND DROPPED MARKER
+     *          ADDED BY: MrSwirlyEyes
+     *                      4/26
+     *
+     *****/
+    /*
+        Takes in Marker object (from after use long-click drops),
+            then creates a circle around it to display the 1/10th mile radius
+     */
+    void addMarkerCircle(Marker marker) {
+        // Determines marker position (lat,long) coordinates
+//        LatLng favLocMarker = marker.getPosition();
+
+        // Circle object with particular options
+//        CircleOptions markerRadius = new CircleOptions()
+//                .center(favLocMarker)       // The marker to create the circle around (or at its center) based on LatLng (coordinates)
+//                .center(marker.getPosition())
+//                .radius(ONE_TENTH_MILE)     // Creates circle of radius ONE_TENTH_MILE (takes in meter value)
+//                .fillColor(0x88AABBCC)      // Circle fill colour is 88 (partially transparent) with AABBCC (color code), default is black 0xFF000000
+//                .strokeWidth(10.0f)         // Width of the stroke (circle outline), default is 10
+//                .strokeColor(0x00000000);   // Circle outline is transparent (the first 2 00's --> transparency)
+
+//        Circle circle = mMap.addCircle(markerRadius);   // In case youwant to act on circle (like set visibility), default is transparent 0x00000000
+//        mMap.addCircle(markerRadius);   // Add's circle radius effect to the map around the dropped marker
+
+        // Circle object with particular options, adds it to map around marker determined by position
+        mMap.addCircle(/*CircleOptions markerRadius = */new CircleOptions()
+//                      .center(favLocMarker)       // The marker to create the circle around (or at its center) based on LatLng (coordinates)
+                  .center(marker.getPosition())
+                    .radius(ONE_TENTH_MILE)     // Creates circle of radius ONE_TENTH_MILE (takes in meter value)
+                    .fillColor(0x88AABBCC)      // Circle fill colour is 88 (partially transparent) with AABBCC (color code), default is black 0xFF000000
+                    .strokeWidth(0.0f)         // Width of the stroke (circle outline), default is 10
+                    .strokeColor(0x00000000)   // Circle outline is transparent (the first 2 00's --> transparency)
+        );
     }
 
 
