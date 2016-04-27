3409959de8eb4cf355db446bd173169eeeea74c9
MrSwirlyEyes <cvjkeef@gmail.com>
2016-04-27 15:17:42 -0700

    NEEDS REFACTORING, and slightly buggy, but needed to push

--- app/src/main/java/com/cse110/team36/coupletones/MapsActivity.java
+++ app/src/main/java/com/cse110/team36/coupletones/MapsActivity.java
@@ -23,7 +23,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import com.google.android.gms.location.LocationListener;
+import android.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -34,6 +34,8 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.sql.Time;
 import java.sql.Timestamp;
@@ -51,10 +53,14 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
     //Temporary until we get DB
     ArrayList<LatLng> locList = new ArrayList<LatLng>();
 
-    double latitude;
-    double longitude;
+//    public static double gpsLatitude = 32.8801;
+//    public static double gpsLongitude = -117.2340;
+    public static double gpsLatitude = 0;
+    public static double gpsLongitude = 0;
+    float gpsPoint[] = new float[2];
     LatLng gpsPos;
-
+    Polyline polyline;
+    PolylineOptions polylineOptions = new PolylineOptions();
 
     // The dialog fragment receives a reference to this Activity through the
     // Fragment.onAttach() callback, which it uses to call the following methods
@@ -124,27 +130,30 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
         mMap.setOnMapLongClickListener(this);   // LISTENER FOR THE LONG-CLICK SO MARKER DROPS ON HELD LOCATION
 
 
+
+
         /***** Add a marker at UCSD and move the camera
          *          ADDED BY: MrSwirlyEyes
          *                      4/25
          *
          *****/
         //FROM HERE
-        LatLng ucsd = new LatLng(32.8801, -117.2340);       // GPS COORDS OF UCSD
-        mMap.addMarker(new MarkerOptions().position(ucsd).title("Marker in UCSD")); // MARKER OPTIONS with TITLE and POSITION @ GPS COORDS OF UCSD
+//        LatLng ucsd = new LatLng(32.8801, -117.2340);       // GPS COORDS OF UCSD
+//        mMap.addMarker(new MarkerOptions().position(ucsd).title("Marker in UCSD")); // MARKER OPTIONS with TITLE and POSITION @ GPS COORDS OF UCSD
 
         mMap.getUiSettings().setZoomControlsEnabled(false); //Disable zoom toolbar
         mMap.getUiSettings().setMapToolbarEnabled(false);   //Disable (useless) map toolbar (literally is garbage)
 
-        float zoomLevel = 15; // Sets default zoom level (instead of seeing world, zooms to UCSD) [This goes up to 21]
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ucsd, zoomLevel));    // MOVES CAMERA THEN ZOOMS TO SET ZOOM LEVEL
+//        float zoomLevel = 15; // Sets default zoom level (instead of seeing world, zooms to UCSD) [This goes up to 21]
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ucsd, zoomLevel));    // MOVES CAMERA THEN ZOOMS TO SET ZOOM LEVEL
 
 
 
 
         //GPS
-        final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         String locationProvider = LocationManager.GPS_PROVIDER;
+//        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
 
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED&&ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -172,45 +181,49 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
                 Criteria criteria = new Criteria();
 
                 // Get the name of the best provider
-                String provider = locationManager.getBestProvider(criteria, true);
+                //String provider = locationManager.getBestProvider(criteria, true);
                 Location myLocation = null;
                 try {
-                    if (mMap != null)
-                        myLocation = locationManager.getLastKnownLocation(provider);
+                    if (mMap != null) {}
+                //        myLocation = locationManager.getLastKnownLocation(provider);
                 } catch (SecurityException e) {
                     Log.e("PERMISSION_EXCEPTION", "PERMISSION_NOT_GRANTED");
                 }
                 // Get latitude of the current location
-                latitude = myLocation.getLatitude();
+//                gpsLongitude = myLocation.getLatitude();
+                MapsActivity.gpsLatitude = location.getLatitude();
 
                 // Get longitude of the current location
-                longitude = myLocation.getLongitude();
-
+//                gpsLongitude = myLocation.getLongitude();
+                MapsActivity.gpsLongitude = location.getLongitude();
                 // Create a LatLng object for the current location
-                gpsPos = new LatLng(latitude, longitude);
-
-
+                gpsPos = new LatLng(MapsActivity.gpsLatitude, MapsActivity.gpsLongitude);
+                String string = "" + String.valueOf(location.getLatitude()) + ", " + String.valueOf(location.getLongitude()) + "\n";
+                Toast.makeText(getBaseContext(), string, Toast.LENGTH_LONG).show();
+                polylineOptions.add(new LatLng(location.getLatitude(),location.getLongitude()));
+                polyline = mMap.addPolyline(polylineOptions);
 //                marker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
 //                mMap.moveCamera(CameraUpdateFactory.newLatLng(gpsPos));
             }
 
-//            @Override
+            @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
 
             }
 
-//            @Override
+            @Override
             public void onProviderEnabled(String provider) {
 
             }
 
-//            @Override
+            @Override
             public void onProviderDisabled(String provider) {
 
             }
         };
 
-//        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+
 
     }
 
@@ -220,6 +233,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
      *
      *****/
     public void onMapLongClick(LatLng point) {
+
 //        Criteria criteria = new Criteria();
 //        final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 //        String provider = locationManager.getBestProvider(criteria, true);
@@ -248,12 +262,29 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
 //        abs_x = abs_x * abs_x;
 //        float abs_y = Math.abs((float) point.longitude - (float) gpsPos.longitude);
 //        abs_y = abs_y * abs_y;
+//        double gps_x = gpsPos.latitude;
+//        float gps_y = (float) gpsPos.longitude
+
+
+        double xx = gpsLatitude - point.latitude;
+        double x_sq = xx * xx;
+        double yy = gpsLongitude - point.longitude;
+        double y_sq = yy * yy;
+//        double dist = Math.sqrt(x_sq + y_sq);
+
+        SphericalUtil distance = new SphericalUtil();
+        double dist = distance.computeDistanceBetween(new LatLng(gpsLatitude,gpsLongitude),point);
 
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
 
-        String markerCoords = "t = " + timeStamp + "\nx =" + String.valueOf(point.latitude) + "\ny=" + point.longitude;
+        polylineOptions.add(point);
+        polyline = mMap.addPolyline(polylineOptions);
 
-        Toast.makeText(MapsActivity.this, markerCoords, Toast.LENGTH_LONG).show();
+//        String markerCoords = "t = " + timeStamp + "\nx =" + gpsLatitude + "\ny=" + gpsLongitude;
+                String markerCoords = "D= " + dist + "\nLat=" + xx + "\nLong=" + yy + "\ngpsLat=" + MapsActivity.gpsLatitude + "\ngpsLong" + MapsActivity.gpsLongitude;
+//        String markerCoords = "dist=" + dist;
+
+//        Toast.makeText(MapsActivity.this, markerCoords, Toast.LENGTH_LONG).show();
 //        if ( Math.sqrt(abs_x + abs_y) > ONE_TENTH_MILE ) {
         // Get instance of Vibrator from current Context
             Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
@@ -261,24 +292,42 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
             // Vibrate for 400 milliseconds
             v.vibrate(100);
 
+            if ( dist > ONE_TENTH_MILE ) {
+                // Marker object, drops where long click has occured
+                Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude)));
+                // Special dropping effect
+                dropPinEffect(marker);
+                // Add circle around marker to display the 1/10th of a mile radius
+//                addMarkerCircle(marker);
 
-            // Marker object, drops where long click has occured
-            Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude)));
-            // Special dropping effect
-            dropPinEffect(marker);
-            // Add circle around marker to display the 1/10th of a mile radius
-            addMarkerCircle(marker);
-
+                Circle circle = mMap.addCircle(new CircleOptions()
+                        .center(marker.getPosition())
+                        .radius(ONE_TENTH_MILE)     // Creates circle of radius ONE_TENTH_MILE (takes in meter value)
+                        .fillColor(0x88AABBCC)      // Circle fill colour is 88 (partially transparent) with AABBCC (color code), default is black 0xFF000000
+                        .strokeWidth(0.0f)         // Width of the stroke (circle outline), default is 10
+                        .strokeColor(0x00000000));   // Circle outline is transparent (the first 2 00's --> transparency));
         /* Open dialog box for saving location
          *          Added by WigginWannabe 26 Apr 2016
          */
-            LocationDialog locationDialog = new LocationDialog();
-            locationDialog.setArguments(new Bundle());
-            locationDialog.show(getFragmentManager(), "set location");
+                LocationDialog locationDialog = new LocationDialog();
+                locationDialog.setArguments(new Bundle());
+                locationDialog.show(getFragmentManager(), "set location");
 
-            // Stazia: I am not finished implementing it yet, but I expected that locations team will have
-            // the new name available to them around here, for creating a new location
+                // Stazia: I am not finished implementing it yet, but I expected that locations team will have
+                // the new name available to them around here, for creating a new location
 //        }
+
+//                Location.distanceBetween(marker.getPosition().latitude, marker.getPosition().longitude,
+
+                Location.distanceBetween( MapsActivity.gpsLatitude, MapsActivity.gpsLongitude,
+                        circle.getCenter().latitude, circle.getCenter().longitude, gpsPoint);
+                Toast.makeText(getBaseContext(), String.valueOf(gpsPoint[0]), Toast.LENGTH_LONG).show();
+//                if( gpsPoint[0] > circle.getRadius()  ){
+//                    Toast.makeText(getBaseContext(), "Outside", Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(getBaseContext(), "Inside", Toast.LENGTH_LONG).show();
+//                }
+            }
     }
 
 
@@ -332,15 +381,85 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
      */
     void addMarkerCircle(Marker marker) {
         // Circle object with particular options, adds it to map around marker determined by position
-        mMap.addCircle(/*CircleOptions markerRadius = */new CircleOptions()
-                .center(marker.getPosition())
-                .radius(ONE_TENTH_MILE)     // Creates circle of radius ONE_TENTH_MILE (takes in meter value)
-                .fillColor(0x88AABBCC)      // Circle fill colour is 88 (partially transparent) with AABBCC (color code), default is black 0xFF000000
-                .strokeWidth(0.0f)         // Width of the stroke (circle outline), default is 10
-                .strokeColor(0x00000000)   // Circle outline is transparent (the first 2 00's --> transparency)
-        );
+
+
+
+
+
     }
 
+    public class SphericalUtil {
+
+        private SphericalUtil() {
+        }
+
+        static final double EARTH_RADIUS = 6371009;
+
+        /**
+         * Returns the angle between two LatLngs, in radians. This is the same as the distance
+         * on the unit sphere.
+         */
+        double computeAngleBetween(LatLng from, LatLng to) {
+            return distanceRadians(Math.toRadians(from.latitude), Math.toRadians(from.longitude),
+                    Math.toRadians(to.latitude), Math.toRadians(to.longitude));
+        }
+
+        /**
+         * Returns distance on the unit sphere; the arguments are in radians.
+         */
+        private double distanceRadians(double lat1, double lng1, double lat2, double lng2) {
+            return arcHav(havDistance(lat1, lat2, lng1 - lng2));
+        }
+
+        public double computeDistanceBetween(LatLng from, LatLng to) {
+            return computeAngleBetween(from, to) * EARTH_RADIUS;
+        }
+
+        /**
+         * Wraps the given value into the inclusive-exclusive interval between min and max.
+         *
+         * @param n   The value to wrap.
+         * @param min The minimum.
+         * @param max The maximum.
+         */
+        double wrap(double n, double min, double max) {
+            return (n >= min && n < max) ? n : (mod(n - min, max - min) + min);
+        }
+
+        /**
+         * Returns the non-negative remainder of x / m.
+         *
+         * @param x The operand.
+         * @param m The modulus.
+         */
+        double mod(double x, double m) {
+            return ((x % m) + m) % m;
+        }
+
+        /**
+         * Computes inverse haversine. Has good numerical stability around 0.
+         * arcHav(x) == acos(1 - 2 * x) == 2 * asin(sqrt(x)).
+         * The argument must be in [0, 1], and the result is positive.
+         */
+        double arcHav(double x) {
+            return 2 * Math.asin(Math.sqrt(x));
+        }
+
+        /**
+         * Returns hav() of distance from (lat1, lng1) to (lat2, lng2) on the unit sphere.
+         */
+        double havDistance(double lat1, double lat2, double dLng) {
+            return hav(lat1 - lat2) + hav(dLng) * Math.cos(lat1) * Math.cos(lat2);
+        }
+        /**
+         * Returns haversine(angle-in-radians).
+         * hav(x) == (1 - cos(x)) / 2 == sin(x / 2)^2.
+         */
+        double hav(double x) {
+            double sinHalf = Math.sin(x * 0.5);
+            return sinHalf * sinHalf;
+        }
+    }
 
 }
 
