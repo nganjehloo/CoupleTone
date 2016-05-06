136ccdff3d5179caa192751744538ffa66627ee8
WigginWannabe <stazia@tronboll.us>
2016-05-06 12:45:14 -0700

    Create FaveLocationManager, but not yet integrated into MapsActivity

--- app/src/main/java/com/cse110/team36/coupletones/FaveLocationManager.java
+++ app/src/main/java/com/cse110/team36/coupletones/FaveLocationManager.java
@@ -1,7 +1,43 @@
 package com.cse110.team36.coupletones;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
 /**
- * Created by stazia on 5/6/16.
+ * Created by stazia on 5/5/16.
  */
-public class FaveLocationManager implements Constants {
+public class FaveLocationManager {
+    public ArrayList<FaveLocation> locList = new ArrayList<FaveLocation>();
+    Context context;
+
+    public FaveLocationManager(Context context) {
+        this.context = context;
+    }
+
+    public void addLocation(String name, LatLng loc) {
+        FaveLocation newLoc = new FaveLocation(name, loc);
+        locList.add(newLoc);
+        Toast.makeText(context,
+                "" + newLoc.getName() + "\n" + String.valueOf(newLoc.getLat()) + ", " +
+                        String.valueOf(newLoc.getLng()), Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean removeLocation(String name) {
+        for (int i = 0; i < locList.size(); i++) {
+            FaveLocation faveLocation = locList.get(i);
+            if (faveLocation.getName().equals(name)) {
+                locList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void emptyLocs() {
+        locList.clear();
+    }
 }
