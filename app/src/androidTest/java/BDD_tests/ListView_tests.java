0fe2d5f946d0bccd1fbe01f200417809b125e8a6
MrSwirlyEyes <cvjkeef@gmail.com>
2016-05-08 13:04:00 -0700

    Starting test for ListView

--- app/src/androidTest/java/BDD_tests/ListView_tests.java
+++ app/src/androidTest/java/BDD_tests/ListView_tests.java
@@ -1,14 +1,28 @@
 package BDD_tests;
 
+import android.app.FragmentManager;
+import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.cse110.team36.coupletones.FaveLocation;
 import com.cse110.team36.coupletones.HomeScreen;
+import com.cse110.team36.coupletones.MyCustomAdapter;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
 
 /**
  * Created by stazia on 5/7/16.
  */
 public class ListView_tests extends ActivityInstrumentationTestCase2<HomeScreen> {
     HomeScreen homeScreen;
+    private ArrayList<FaveLocation> list = new ArrayList<>();
+    LatLng equator = new LatLng(0.000000,0.000000);
+    LatLng ucsd = new LatLng(32.000000,117.000000);
+    LatLng oc = new LatLng(34.000000,118.000000);
+    private Context context;
+    private FragmentManager fragmentManager;
+
 
     public ListView_tests() {
         super(HomeScreen.class);
@@ -19,6 +33,9 @@ public class ListView_tests extends ActivityInstrumentationTestCase2<HomeScreen>
         homeScreen = getActivity();
         // add location to locList
         // get custom adapter
+        list.add("Equator",Double.valueOf(equator.latitude), Double.valueOf(equator.longitude));
+
+        MyCustomAdapter adapter = new MyCustomAdapter(list, context, fragmentManager);
         assertEquals(adapter.getCount(), 1);
     }
 
@@ -28,6 +45,7 @@ public class ListView_tests extends ActivityInstrumentationTestCase2<HomeScreen>
         // add location to locList
         // get custom adapter
         // remove location from locList
+        MyCustomAdapter adapter = new MyCustomAdapter(list, context, fragmentManager);
         assertEquals(adapter.getCount(), 0);
     }
 
