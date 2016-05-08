3c27977a63bc8d4386c2ced4e26b97ad3ce99adf
hungale <hungale@github.com>
2016-05-09 06:44:51 +0900

    m

--- app/src/main/java/com/cse110/team36/coupletones/FaveLocation.java
+++ app/src/main/java/com/cse110/team36/coupletones/FaveLocation.java
@@ -1,21 +1,26 @@
+/*
+ * Class name: FaveLocation.java
+ * Purpose: This class is used in FaveLocationManager and the rest of the program to represent
+ * a SO's favorite location, which consists of a name and a latitude and longitude.
+ */
 package com.cse110.team36.coupletones;
 
 import com.google.android.gms.maps.model.LatLng;
 
-/**
- *
- */
 public class FaveLocation {
+    // data member variables
     private double lat;
     private double lon;
     private String name;
 
+    // constructor
     public FaveLocation(String name, LatLng coords) {
         this.lat = coords.latitude;
         this.lon = coords.longitude;
         this.name = name;
     }
 
+    // accessors
     public double getLat() {
         return lat;
     }
@@ -30,6 +35,7 @@ public class FaveLocation {
 
     public String getName() { return name; }
 
+    // mutators
     protected void setName(String name) {
         this.name = name;
     }
