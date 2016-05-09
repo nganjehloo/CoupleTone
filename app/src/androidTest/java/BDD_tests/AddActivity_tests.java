e2e4d17899bc6eee2961793fcc266d7d26310279
nganjehloo <nganjehloo@gmail.com>
2016-05-08 17:37:07 -0700

    idk

--- app/src/androidTest/java/BDD_tests/AddActivity_tests.java
+++ app/src/androidTest/java/BDD_tests/AddActivity_tests.java
@@ -10,17 +10,15 @@ import com.cse110.team36.coupletones.HomeScreen;
  * Created by stazia on 5/5/16.
  */
 public class AddActivity_tests extends ActivityInstrumentationTestCase2<SOActivity> {
-//    Activity soActivity ;
-    SOActivity soActivity;
-//    HomeScreen soActivity;
+//      Activity soActivity ;
+        SOActivity soActivity;
+//      HomeScreen soActivity;
         public AddActivity_tests() {
             super(SOActivity.class);
         }
 
     public void test_addSO() {
         soActivity = getActivity();
-
-
 
     }
 }
