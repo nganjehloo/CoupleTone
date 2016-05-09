68f49540c64cc20efbb7f8b9d36c65308d3f27c2
nganjehloo <nganjehloo@gmail.com>
2016-05-08 19:19:54 -0700

    deg

--- app/src/androidTest/java/BDD_tests/Notify_tests.java
+++ app/src/androidTest/java/BDD_tests/Notify_tests.java
@@ -2,14 +2,19 @@ package BDD_tests;
 
 import com.cse110.team36.coupletones.FaveLocation;
 import com.google.android.gms.maps.model.LatLng;
-
+import com.cse110.team36.coupletones.Constants;
 /**
  * Created by Nima on 5/8/2016.
  */
-public class Notify_tests {
+public class Notify_tests implements Constants{
+    LatLng[] testPoints = { new LatLng(0,0),
+            new LatLng(ONE_TENTH_MILE/2, ONE_TENTH_MILE/2),
+            new LatLng(ONE_TENTH_MILE, ONE_TENTH_MILE),
+            new LatLng(ONE_TENTH_MILE+1, ONE_TENTH_MILE+1)};
 
     public void test_OutsideRange(){
-        FaveLocation faveLocation = new FaveLocation("testloc", new LatLng() )
+        FaveLocation faveLocation = new FaveLocation("testloc", testPoints[1]);
+
     }
 
 }
