3fe02caac6384327726f471d3060239179f21f85
Duc Le <dqle123@gmail.com>
2016-05-08 20:20:51 -0700

    s

--- app/src/androidTest/java/BDD_tests/Notify_tests.java
+++ app/src/androidTest/java/BDD_tests/Notify_tests.java
@@ -5,14 +5,19 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.cse110.team36.coupletones.FaveLocation;
+import com.cse110.team36.coupletones.FaveLocationManager;
 import com.cse110.team36.coupletones.GCM.MyGCMListenerService;
 import com.cse110.team36.coupletones.GCM.SOConfig;
 import com.cse110.team36.coupletones.GCM.sendNotificationJob;
+import com.cse110.team36.coupletones.MapManager;
+import com.cse110.team36.coupletones.MapsActivity;
 import com.cse110.team36.coupletones.R;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
 import com.google.android.gms.maps.model.LatLng;
 import com.cse110.team36.coupletones.Constants;
+import com.google.maps.android.SphericalUtil;
+
 import android.test.ActivityInstrumentationTestCase2;
 
 import java.io.IOException;
@@ -20,13 +25,13 @@ import java.io.IOException;
 /**
  * Created by Nima on 5/8/2016.
  */
-public class Notify_tests extends ActivityInstrumentationTestCase2<SOConfig> implements Constants  {
+public class Notify_tests extends ActivityInstrumentationTestCase2<MapsActivity> implements Constants  {
     LatLng[] testPoints = { new LatLng(0,0),
             new LatLng(ONE_TENTH_MILE/2, ONE_TENTH_MILE/2),
             new LatLng(ONE_TENTH_MILE, ONE_TENTH_MILE),
             new LatLng(ONE_TENTH_MILE+1, ONE_TENTH_MILE+1)};
 
-    public Notify_tests(){super(SOConfig.class);};
+    public Notify_tests(){super(MapsActivity.class);};
 
    // public void test_OutsideRange(){
       //  FaveLocation faveLocation = new FaveLocation("testloc", testPoints[1]);
@@ -37,19 +42,22 @@ public class Notify_tests extends ActivityInstrumentationTestCase2<SOConfig> imp
         new AsyncTask<Void, Void, String>() {
 
             FaveLocation faveLocation = new FaveLocation("testloc", testPoints[1]);
-            SOConfig soConfig = getActivity();
+            MapsActivity map = getActivity();
             String token;
 
             @Override
             protected String doInBackground(Void... params) {
-                String message = "SEND THIS NOTIFICATION";
-                InstanceID instanceID = InstanceID.getInstance(soConfig.getApplicationContext());
 
+                //Get Reg ID
+                InstanceID instanceID = InstanceID.getInstance(map.getApplicationContext());
                 try {
                     token = instanceID.getToken("755936681526", GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                //equation for distance
+                int dist = SphericalUtil.computeDistanceBetween(FaveLocationManager.locList.get(i).getCoords(), mapsManager.getGPSPos());
 
                 return message;
             }
