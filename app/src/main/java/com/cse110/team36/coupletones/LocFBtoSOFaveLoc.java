eebc2f8db71f6a5def3c645ca7d0ef48b8f25622
hungale <hungale@github.com>
2016-05-22 09:24:16 +0900

    temp fix

---
+++ app/src/main/java/com/cse110/team36/coupletones/LocFBtoSOFaveLoc.java
@@ -0,0 +1,13 @@
+package com.cse110.team36.coupletones;
+
+import com.cse110.team36.coupletones.FireBase.LocationFB;
+
+/**
+ * Created by aspidiske on 5/22/2016.
+ */
+public class LocFBtoSOFaveLoc extends SOFaveLoc {
+    LocFBtoSOFaveLoc(LocationFB locationFB){
+        super();
+
+    }
+}
