5c67c855e9a53ea1714304262415a2a626d1c220
hungale <hungale@github.com>
2016-06-04 08:34:47 +0900

    working on SO list test

---
+++ app/src/androidTest/java/BDD_tests/ViewSOList_tests.java
@@ -0,0 +1,40 @@
+package BDD_tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.cse110.team36.coupletones.lists.SOListActivity;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+
+public class ViewSOList_tests extends ActivityInstrumentationTestCase2<SOListActivity> {
+    public ViewSOList_tests(){super(SOListActivity.class);};
+
+    Firebase myFirebaseRef;
+    Firebase soFirebaseRef;
+
+    //receive SO's newly added location
+    public void test_SOAddsLoc(){
+        myFirebaseRef = new Firebase("https://coupletones36.firebaseio.com/" + "SOTESTLISTUSER1" + "/locations");
+        soFirebaseRef = new Firebase("https://coupletones36.firebaseio.com/" + "SOTESTLISTUSER2" + "/locations");
+        soFirebaseRef.child("words").setValue("morewords");
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        })
+
+
+    }
+    //get updated SO's list should they delete
+
+    //be able to see list of SO's list upon opening the apps
+}
