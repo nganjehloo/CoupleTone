4b99c192a71d0b66241a99a1fa7d24a4a47d0d4a
nganjehloo <nganjehloo@gmail.com>
2016-06-03 19:43:07 -0700

    finished tests

---
+++ app/src/androidTest/java/BDD_tests/ResetDailyHist_tests.java
@@ -0,0 +1,26 @@
+package BDD_tests;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by Nima on 6/3/2016.
+ */
+public class ResetDailyHist_tests  {
+    public ResetDailyHist_tests(){}
+
+    public void reset3AM(){
+        long epoch = System.currentTimeMillis()/1000;
+        String time = new java.text.SimpleDateFormat("h:mm:ss a").format(new java.util.Date (epoch*1000));
+
+        assert(time.equals("3:00:00 AM"));
+
+    }
+
+    public void noreset3AM(){
+        long epoch = System.currentTimeMillis()/1000;
+        String time = new java.text.SimpleDateFormat("h:mm:ss a").format(new java.util.Date (epoch*1000));
+
+        assert(!time.equals("3:00:00 AM"));
+
+    }
+}
