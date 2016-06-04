53b694bb5d775483236ebd8595783806e2fd7f80
nganjehloo <nganjehloo@gmail.com>
2016-06-03 17:36:48 -0700

    more tests

--- app/src/androidTest/java/BDD_tests/NotifOptions_tests.java
+++ app/src/androidTest/java/BDD_tests/NotifOptions_tests.java
@@ -2,17 +2,39 @@ package BDD_tests;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.cse110.team36.coupletones.FireBase.SOConfig;
 import com.cse110.team36.coupletones.NotifSettings;
+import com.cse110.team36.coupletones.SparkleToneFactory;
+import com.cse110.team36.coupletones.VibeToneFactory;
 import com.cse110.team36.coupletones.maps.MapsActivity;
 
 /**
  * Created by Nima on 6/3/2016.
  */
-public class NotifOptions_tests extends ActivityInstrumentationTestCase2<NotifSettings> {
+public class NotifOptions_tests extends ActivityInstrumentationTestCase2<SOConfig> {
 
-    NotifOptions_tests(){ super(NotifSettings.class);}
+
+    SOConfig soConfig;
+    NotifOptions_tests(){ super(SOConfig.class);}
 
     public void test_playSound(){
-
+        assertEquals(true, (new SparkleToneFactory()).getSparkEnable());
     }
+
+    public void test_playNoSound(){
+        SparkleToneFactory.setSparkEnable();
+        assertEquals(false, (new SparkleToneFactory()).getSparkEnable());
+    }
+
+    public void test_playVIbrate(){
+        soConfig = getActivity();
+        assertEquals(true, (new VibeToneFactory(soConfig)).getVibeEnable());
+    }
+
+    public void test_playNoVIbrate(){
+        soConfig = getActivity();
+        VibeToneFactory.setVibeEnable();
+        assertEquals(false, (new VibeToneFactory(soConfig)).getVibeEnable());
+    }
+
 }
